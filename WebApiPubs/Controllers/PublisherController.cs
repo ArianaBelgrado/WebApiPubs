3c78cb1bbc02c27c5bbcf0c55995d6cab01529f3
Ariana Belgrado <belgradoariana@gmail.com>
2023-01-26 02:27:04 -0300

    Agregar archivos de proyecto.

---
+++ WebApiPubs/Controllers/PublisherController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiPubs.Models;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private readonly pubsContext context;
+
+        public PublisherController(pubsContext context)
+        {
+            this.context = context;
+        }
+        // Get
+        [HttpGet]
+        public ActionResult<IEnumerable<Publishers>> GetClinica()
+        {
+            return context.Publishers.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Publishers> GetbyId(string id)
+        {
+            Publishers publisher = (from p in context.Publishers
+                                     where p.PubId == id
+                                     select p).SingleOrDefault();
+
+            return publisher;
+
+        }
+
+        //UPDATE
+        //PUT api/publisher/{id}
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, Publishers publisher)
+        {
+            if (id != publisher.PubId)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(publisher).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //INSERT
+        [HttpPost]
+        public ActionResult Post(Publishers publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            context.Publishers.Add(publisher);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //DELETE
+        //DELETE api/publisher/{id}
+        [HttpDelete("{id}")]
+        public ActionResult<Publishers> Delete(string id)
+        {
+            var publisher = (from p in context.Publishers
+                             where p.PubId == id
+                             select p).SingleOrDefault();
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            context.Publishers.Remove(publisher);
+            context.SaveChanges();
+
+            return publisher;
+        }
+    }
+}
---
+++ WebApiPubs/Controllers/StoreController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiPubs.Models;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly pubsContext context;
+
+        public StoreController(pubsContext context)
+        {
+            this.context = context;
+        }
+        // Get
+        [HttpGet]
+        public ActionResult<IEnumerable<Sales>> GetClinica()
+        {
+            return context.Sales.ToList();
+        }
+        //UPDATE
+        //PUT api/store/{id}
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, Sales Sales)
+        {
+            if (id != Sales.StorId)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(Sales).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //INSERT
+        [HttpPost]
+        public ActionResult Post(Sales Sales)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            context.Sales.Add(Sales);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //DELETE
+        //DELETE api/store/{id}
+        [HttpDelete("{id}")]
+        public ActionResult<Sales> Delete(string id)
+        {
+            var Sales = (from c in context.Sales
+                             where c.StorId == id
+                             select c).SingleOrDefault();
+
+            if (Sales == null)
+            {
+                return NotFound();
+            }
+
+            context.Sales.Remove(Sales);
+            context.SaveChanges();
+
+            return Sales;
+        }
+    }
+}
